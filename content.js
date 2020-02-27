922599a0a4b061df65d26188b844949b564bd9f2
tyghtmo <tyson@Tysons-MBP.modem>
2020-02-27 19:48:18 +1100

    Time is money, Initial commit

---
+++ content.js
@@ -0,0 +1,37 @@
+var elements = document.getElementsByTagName('*');
+var hourlyPay = 29;
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        var text = node.nodeValue;
+
+        if (text != null) {
+            var pattern = /\$\d+\.?\d{0,2}/;
+            var numString = "";
+
+            var match = text.match(pattern);
+            if (match != null) {
+                numString = match[0];
+                numString = text.replace('$', '');
+                numString = numString.trim();
+                numString = numString.split(' ')[0];
+                //alert(numString);
+                numString = numString / hourlyPay;
+                numString = numString.toFixed(2) + ' Hours';
+
+                //alert(numString);
+                //var replacedText = text + " " + numString;
+                var replacedText = numString;
+                //var replacedText = text.replace('$', 'hours');
+    
+                if (replacedText !== text) {
+                    element.replaceChild(document.createTextNode(replacedText), node);
+                }
+            }
+        }
+    }
+}
